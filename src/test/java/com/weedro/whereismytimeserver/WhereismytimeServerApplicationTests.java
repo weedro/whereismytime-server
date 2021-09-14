4999b267877aac2b682833692fb6fa13dafa1170
blackkiv <fallena238@gmail.com>
2021-09-14 23:40:43 +0300

    init

---
+++ src/test/java/com/weedro/whereismytimeserver/WhereismytimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weedro.whereismytimeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereismytimeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
