3d5e8b5e5ae674b140bd1d50c7b94660000925af
Livio Agolini <fallena238@gmail.com>
2022-05-28 18:08:49 +0200

    fix?????????

--- app/whereismytime-service/src/main/java/com/weedro/whereismytime/controller/WastedTimeController.java
+++ app/whereismytime-service/src/main/java/com/weedro/whereismytime/controller/WastedTimeController.java
@@ -7,6 +7,7 @@ import com.weedro.whereismytime.service.WastedTimeService;
 import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(Api.BASE_URL + "/track")
 public record WastedTimeController(WastedTimeService wastedTimeService) {
